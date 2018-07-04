81c128ac13d4affef0679bddbdfcced893920a2b
herison <herison.assuncao@outlook.com>
2018-07-04 01:41:37 -0400

    Modificação dos nomes das view, e construção do html e css

---
+++ cursou_layout.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+    <title>Declaração</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style_declaracao.css">
+</head>
+
+<body>
+<div class="container bloco ">
+    <div>
+        <h3 class="text-center" >LOGO DA FACULDADE</h3>
+        <img src="" alt="">
+    </div>
+    <div class="row">
+        <h3 class="text-center" id="titulo"> <b>DECLARAÇÃO</b> </h3>
+        <p class="paragrafo">Declaramos, para os devidos fins, que <span style="font-weight:600">HÉRISON ROQUE DE ASSUNÇÃO</span>, CPF 000.000.000-65, RG 0000000-0, matrícula <spam style="font-weight: 600">135465461464</spam>, aluno(a) desta Instituição de Ensino, cursou o 4º Périodo  do Curso de <span style="font-weight: 600">CIÊNCIA DA COMPUTAÇÃO</span>, turno <span style="font-weight: 600">Noturno.</span></p>
+
+    </div>
+
+    <div class="row bloco-data">
+
+        <p class="text-right">Manaus, 4 de julho de 2016 </p><br>
+        <p class="text-right">Assinatura</p>
+        <p class="text-right">Secretária</p>
+    </div>
+
+    <div class="row bloco-rodape">
+
+        <p class="text-center ">
+            <span style="font-weight: 600"><br>NOME DA FACULDADE</span><br>
+            <span style="font-weight: 600">Endereço</span><br>
+            <span style="font-weight: 600">CEP - Manaus - AM - Brasil</span><br>
+            <span style="font-weight: 600">TEL: 000000000000</span><br>
+            <span style="font-weight: 600">Site</span><br>
+            <span style="font-weight: 600">Dada de Validade do documento</span><br>
+        </p>
+
+    </div>
+
+</div>
+</body>
+
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</html>
