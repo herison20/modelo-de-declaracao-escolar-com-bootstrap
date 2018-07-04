b1e068f35bf63ce05aaadac2fb95246187186d9e
herison <herison.assuncao@outlook.com>
2018-07-03 23:45:36 -0400

    Arquivos base para o projeto

---
+++ matriz.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+ 
+<html lang="pt-br">
+<head>
+    <title> Matriz Curricular</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style_historico.css">
+</head>
+
+<body>
+    <div class="container bloco">
+
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <p><strong><u><span>CURSO</span> BACHARELADO EM JORNALISMO</u>
+            <p>Renovação de Reconhecimento pela Portaria MEC Nº 267 de <br> 03/04/2017, publicada no DOU de 04/04/2017
+            <p><u>MATRIZ CURRICULAR</u></strong></p></p></p>
+        </div>
+    </div>
+    <hr>
+        
+    <table class="table  table-bordered table-condensed ">
+        <thead class="" >
+            <tr bgcolor="#c6d9f1" >
+                <th width="1%" class="text-center">SEMESTRE</th>
+                <th width="5%" class="text-center">COMPONENTES CURRICULARES CURSADOS</th>
+                <th width="1%" class="text-center">CARGA HORÁRIA</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td class="text-center">1º</td>
+                <td class="">INTRODUÇÃO  AO  JORNALISMO</td>
+                <td class="text-center">40</td>
+            </tr>
+            <tr>
+                <td class="text-center">2º</td>
+                <td class="">INTRODUÇÃO  AO  JORNALISMO</td>
+                <td class="text-center">40</td>
+            </tr>
+            <tr>
+                <td class="text-center">3º</td>
+                <td class="">INTRODUÇÃO  AO  JORNALISMO</td>
+                <td class="text-center">40</td>
+            </tr>
+            <tr>
+                <td colspan="2" class="text-center"><label> &emsp;CARGA HORÁRIA TOTAL</label></td>
+                <td colspan="1" class="text-center"><strong>120</strong></td>
+            </tr>
+            
+           
+        </tbody>
+    </table>
+
+    
+                   
+
+</div>
+</body>
+    
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</html>
